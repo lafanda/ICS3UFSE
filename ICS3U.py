24a2845d6ab20ea4914d8d0ea2420290b3381ca9
NlMBUS <63270643+NlMBUS@users.noreply.github.com>
2020-04-20 21:06:43 -0400

    Update ICS3U.py

--- ICS3U.py
+++ ICS3U.py
@@ -9,13 +9,21 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 myclock = time.Clock()
 
-class Player:
+class Object:
+    def render(self):
+        '''rendering object'''
+        pass
+    
+class Player(Object):
     def __init__(self,x,y,face):
         playerPic=image.load('C:/Users/Abid/Documents/GitHub/ICS3UFSE/images/Sprite.png')
         self.x = x
         self.y = y
         self.face = face
         self.sprite = playerPic
+        self.hurtbox = Rect(self.x-13,self.y-13, 25, 25)
+        self.hitbox = Rect(self.x-20, self.y, 20, 40)
+        
     def render(self):
         screen.blit(self.sprite,(self.x-13,self.y-13))
     def movement(self):
@@ -36,57 +44,64 @@ class Player:
     def sword(self):
         mb = mouse.get_pressed()
         mx, my = mouse.get_pos()  # move good guy
-        if self.face == "up":
-            swordRect = Rect(self.x, self.y-40, 20, 40)
-        if self.face == "right":
-            swordRect = Rect(self.x, self.y, 40, 20)
-        if self.face == "down":
-            swordRect = Rect(self.x-20, self.y, 20, 40)
-        if self.face == "left":
-            swordRect = Rect(self.x-40, self.y-20, 40, 20)
         if mb[0] == 1:
+            if self.face == "up":
+                swordRect = Rect(self.x, self.y-40, 20, 40)
+            if self.face == "right":
+                swordRect = Rect(self.x, self.y, 40, 20)
+            if self.face == "down":
+                swordRect = Rect(self.x-20, self.y, 20, 40)
+            if self.face == "left":
+                swordRect = Rect(self.x-40, self.y-20, 40, 20)
             draw.rect(screen, BLUE, swordRect)
-        self.hitbox = swordRect
-class Enemy:
+            self.hitbox = swordRect
+    def collision(self, enemy):
+        if self.hurtbox.colliderect(enemy.hurtbox):
+            return True
+        
+class Enemy(Object):
     def __init__(self, enemyx, enemyy):
         enemyPic=image.load('C:/Users/Abid/Documents/GitHub/ICS3UFSE/images/Sprite2.png')
-        delete = True
         self.enemyx = enemyx
         self.enemyy = enemyy
         self.sprite = enemyPic
-        self.delete = delete
         enemyRect = Rect(enemyx, enemyy, 25, 25)
         draw.rect(screen, GREEN, enemyRect)
         self.hurtbox = enemyRect
     def render(self):
         screen.blit(self.sprite,(self.enemyx,self.enemyy))
-    def collision(self):
+    def collision(self, player):
         if player.hitbox.colliderect(self.hurtbox):
-            return self.delete == True
+            return True
 
-class Level:
-    def __init__(self):
-        levels = ["Tutorial", "Level1"]
-        level = "Tutorial"
 
 
 Enemyx = randint(50, 500)
 Enemyy = randint(50, 500)
 player = Player(200,200,"down")
 enemy = Enemy(Enemyx, Enemyy)
+levels = ["Tutorial", "Level1"]
+levelnum = 0
+objects = [player, enemy]
+enemies = [enemy]
 running = True
 while running:
     for evt in event.get():
         if evt.type == QUIT:
             running = False
     screen.fill((0, 0, 0))
-    classes = [player.render(), player.movement(),player.sword(),enemy.render(),enemy.collision()]
-    if enemy.collision() == True:
-        classes.remove(enemy.collision())
-        classes.remove(enemy.render())
-    for i in classes:
-        i
-    print(classes)
+    for obj in objects:
+        obj.render()
+    player.movement()
+    player.sword()
+
+    for enemy in enemies:
+        if enemy.collision(player) == True:
+            enemies.remove(enemy)
+            objects.remove(enemy)
+            levelnum +=1
+    level = levels[levelnum]
+    print(player.collision(enemy))
     display.flip()
     myclock.tick(60)
     
